d81cf31b50beede4902cd7ece9732b310a55ef7f
ayakimov <A.Yakimov@softline.com>
2021-11-13 17:54:44 +0500

    add maps

--- assets/js/bot.js
+++ assets/js/bot.js
@@ -1,6 +1,6 @@
 const buttonMessage = document.querySelector('.button-message')
 const modalAuth = document.querySelector('.modal-auth')
-const closeAuth = document.querySelector('.close-button')
+const closeMessage = document.querySelector('.close-button')
 const logInForm = document.getElementById('logInForm')
 const InputFio = document.getElementById('fio')
 const InputPhone = document.getElementById('phone')
@@ -14,26 +14,33 @@ buttonMessage.addEventListener('click', () => {
   // send()
 })
 
-closeAuth.addEventListener('click', () => {
+closeMessage.addEventListener('click', () => {
   modalAuth.style.display = 'none'
-  // console.dir(buttonMessage)
-  // send()
+  InputFio.value=''
+  InputPhone.value=''
+  InputMessage.value=''
 })
 
 
 
 logInForm.addEventListener('submit', (event) => {
   send(InputFio.value, InputPhone.value, InputMessage.value)
-  // console.log("InputLogin =", InputLogin.value, "InputLogin =", InputPassword.value)
   event.preventDefault()
-  console.dir(event)
-   modalAuth.style.display = 'none'
+  modalAuth.style.display = 'none'
+  InputFio.value=''
+  InputPhone.value=''
+  InputMessage.value=''
 })
 
 const send = (s1, s2, s3) =>{
 var chatid = "-1001621864788";
 var token = "2107046347:AAFl_Gb9XV-6Bt1RfjrVmuVraEGrvQLQprA";
-var text = ("Фио =" + s1+ "\n\nТелефон =" + s2 + "  \n\nСообщение =" + s3)
+var text = ("<b>Фио =  </b>" +"<b>"+s1+"</b>"+"\n<b>Телефон =  </b>" + "<b>"+s2+"</b>" + "\n<b>Сообщение =  </b>" + "<b>"+s3+"</b>")
+
+
+// ("<b>89638823214</b> \n <strong>89638823214</strong>")
+// var text = ("<pre language='89638823214'>code</pre>")
+
 
 //Отправляем текст в наш телеграм канал
 otpravka(token,text,chatid);
@@ -42,7 +49,9 @@ function otpravka(token,text,chatid){
   var z=$.ajax({  
   type: "POST",  
   url: "https://api.telegram.org/bot"+token+"/sendMessage?chat_id="+chatid,
-  data: "parse_mode=HTML&text="+encodeURIComponent(text), 
+  // data: "parse_mode=HTML&text="+encodeURIComponent(text), 
+  data: "parse_mode=HTML&text="+text, 
+
   }); 
  };
  }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	var	$window = $(window),
@@ -79,7 +73,7 @@ $('path').hover(function(e){
     $('<div>' + name +'</div>').appendTo('.indicator');
   }
 
-
+// КАРТА РОССИИ  
   if($(this).attr('flag')) {
     var flag = "http://sandbox.alexti-dev.ru/mapping/" + $(this).attr('flag') ;
     $(' <img class="flag" src="" alt="">').appendTo('.indicator');
@@ -89,6 +83,7 @@ $('path').hover(function(e){
 
  $('.change').remove();
 
+
  var script = document.createElement('script');
  // script.classList.add('change');
   //script.src = 'http://api.geonames.org/countryInfoJSON?country='+info[id]+'&username=pixeltest&style=full&callback=update';
@@ -97,13 +92,10 @@ $('path').hover(function(e){
   $(this).css('fill','#f6e72d');
   $('path').not(this).css('fill','rgba(0,0,0,0.5)');
   $('.indicator').css({'top':e.pageY,'left':e.pageX+30}).show();
-
-
-
 },function(){
   $('.indicator').html('');
   $('.indicator').hide();
-  $('path').css('fill','rgba(0,0,0,0.2)');
+  $('path').css('fill','rgba(0,0,0,0.5)');
 });
 
 
@@ -111,7 +103,7 @@ $('path').hover(function(e){
 
 
 // var idAarr = ["RU-MOW", "RU-SPE", "RU-NEN", "RU-YAR", "RU-CHE", "RU-ULY", "RU-TYU", "RU-TUL", "RU-SVE", "RU-RYA", "RU-ORL", "RU-OMS", "RU-NGR", "RU-LIP", "RU-KRS", "RU-KGN", "RU-KGD", "RU-IVA", "RU-BRY", "RU-AST", "RU-KHA", "RU-CE", "RU-UD", "RU-SE", "RU-MO", "RU-KR", "RU-KL", "RU-IN", "RU-AL", "RU-BA", "RU-AD", "RU-CR", "RU-SEV", "RU-KO", "RU-KIR", "RU-PNZ", "RU-TAM", "RU-MUR", "RU-LEN", "RU-VLG", "RU-KOS", "RU-PSK", "RU-ARK", "RU-YAN", "RU-CHU", "RU-YEV", "RU-TY", "RU-SAK", "RU-AMU", "RU-BU", "RU-KK", "RU-KEM", "RU-NVS", "RU-ALT", "RU-DA", "RU-STA", "RU-KB", "RU-KC", "RU-KDA", "RU-ROS", "RU-SAM", "RU-TA", "RU-ME", "RU-CU", "RU-NIZ", "RU-VLA", "RU-MOS", "RU-KLU", "RU-BEL", "RU-ZAB", "RU-PRI", "RU-KAM", "RU-MAG", "RU-SA", "RU-KYA", "RU-ORE", "RU-SAR", "RU-VGG", "RU-VOR", "RU-SMO", "RU-TVE", "RU-PER", "RU-KHM", "RU-TOM", "RU-IRK"];
-var idAarr = ["RU-MOW", "RU-CHE","RU-TUL"];
+var idAarr = ["RU-MOW", "RU-CHE", "RU-PER", "RU-SVE", "RU-KDA", "RU-UD", "RU-KHA", "RU-KYA", "RU-TYU"];
 
 var idAarr2 = new Array(
   ["RU-MOW",  "Москва", "moscow.gif"],
@@ -129,16 +121,16 @@ var idAarr2 = new Array(
   // ["RU-YAR",  "Ярославская область", ""],
   // ["RU-ULY",  "Ульяновская область", ""],
   // ["RU-NVS",  "Новосибирская область", ""],
-  // ["RU-TYU",  "Тюменская область", ""],
-  // ["RU-SVE",  "Свердловская область", ""],
+  ["RU-TYU",  "Тюменская область", ""],
+  ["RU-SVE",  "Свердловская область", ""],
   // ["RU-NGR",  "Новгородская область", ""],
   // ["RU-KGN",  "Курганская область", ""],
   // ["RU-KGD",  "Калининградская область", ""],
   // ["RU-IVA",  "Ивановская область", ""],
   // ["RU-AST",  "Астраханская область", ""],
-  // ["RU-KHA",  "Хабаровский край", ""],
+  ["RU-KHA",  "Хабаровский край", ""],
   // ["RU-CE",  "Чеченская республика", ""],
-  // ["RU-UD",  "Удмуртская республика", ""],
+  ["RU-UD",  "Удмуртская республика", ""],
   // ["RU-SE",  "Республика Северная Осетия", ""],
   // ["RU-MO",  "Республика Мордовия", ""],
   // ["RU-KR",  "Республика  Карелия", ""],
@@ -169,7 +161,7 @@ var idAarr2 = new Array(
   // ["RU-DA",  "Республика Дагестан", ""],
   // ["RU-KB",  "Кабардино-Балкарская республика", ""],
   // ["RU-KC",  "Карачаевая-Черкесская республика", ""],
-  // ["RU-KDA",  "Краснодарский край", ""],
+  ["RU-KDA",  "Краснодарский край", ""],
   // ["RU-ROS",  "Ростовская область", ""],
   // ["RU-SAM",  "Самарская область", ""],
   // ["RU-TA",  "Республика Татарстан", ""],
@@ -185,14 +177,14 @@ var idAarr2 = new Array(
   // ["RU-KAM",  "Камачатский край", ""],
   // ["RU-MAG",  "Магаданская область", ""],
   // ["RU-SA",  "Республика Саха", ""],
-  // ["RU-KYA",  "Красноярский край", ""],
+  ["RU-KYA",  "Красноярский край", ""],
   // ["RU-ORE",  "Оренбургская область", ""],
   // ["RU-SAR",  "Саратовская область", ""],
   // ["RU-VGG",  "Волгоградская область", ""],
   // ["RU-VOR",  "Ставропольский край", ""],
   // ["RU-SMO",  "Смоленская область", ""],
   // ["RU-TVE",  "Тверская область", ""],
-  // ["RU-PER",  "Пермская область", ""],
+  ["RU-PER",  "Пермский край", ""],
   // ["RU-KHM",  "Ханты-Мансийский АО", ""],
   // ["RU-KHM",  "Ханты-Мансийский АО", ""],
   // ["RU-TOM",  "Томская область", ""],
